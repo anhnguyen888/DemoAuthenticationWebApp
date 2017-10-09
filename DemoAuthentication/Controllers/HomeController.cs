9fc77a655821079a46a892a5f92a2105198b3212
Administrator <Administrator@L8PC167>
2017-10-09 10:16:24 +0700

    Add Authorize To HomeController ADMIN

--- DemoAuthentication/Controllers/HomeController.cs
+++ DemoAuthentication/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace DemoAuthentication.Controllers
 {
+    [Authorize(Roles ="admin")]
     public class HomeController : Controller
     {
         public ActionResult Index()
